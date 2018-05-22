708066ed83e0c3ea8630363ee3c06d22cafdcdae
kromsom <kevin.romsom@gmail.com>
2018-05-22 13:17:12 +0200

    adddrinks pagina

--- Killerapp/Killerapp/Controllers/HomeController.cs
+++ Killerapp/Killerapp/Controllers/HomeController.cs
@@ -13,18 +13,20 @@ namespace Killerapp.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Registreer()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        public ActionResult AddDrinks()
+        {
+            return View();
+        }
+        
     }
 }
