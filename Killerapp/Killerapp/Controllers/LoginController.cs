4adfd6215d38afb2750c21fee17cc1d89d35b26f
kromsom <kevin.romsom@gmail.com>
2018-05-25 11:35:41 +0200

    ui layer  voor login en sql layer voor login

---
+++ Killerapp/Killerapp/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Killerapp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            try
+            {
+                ViewData["Email"] = collection[1];
+                ViewData["Password"] = collection[2];
+
+                return View("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Killerapp/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class User
+    {
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is verplicht")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Wachtwoord")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Wachtwoord is verplicht")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Wachtwoord moet minimaal uit 6 karakter bestaan !")]
+        public string Password { get; set; }
+    }
+}
