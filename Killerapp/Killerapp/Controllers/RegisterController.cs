a6551a045ababc8e3362556e8d78625e0d2fd71d
kromsom <kevin.romsom@gmail.com>
2018-05-27 21:07:03 +0200

    Register page gemaakt

---
+++ Killerapp/Killerapp/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Logic;
+using Models;
+
+namespace Killerapp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Killerapp/Models/RegisterModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Register
+    {
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is verplicht")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Wachtwoord")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Wachtwoord is verplicht")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Wachtwoord moet minimaal uit 6 karakter bestaan !")]
+        public string Password { get; set; }
+
+        [Display(Name = "Herhaal wachtwoord")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Wachtwoord is verplicht")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Wachtwoord moet minimaal uit 6 karakter bestaan !")]
+        public string PasswordRepeat { get; set; }
+
+        [Display(Name = "Voornaam")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Voornaam is verplicht")]
+        [DataType(DataType.Text)]
+        public string Voornaam { get; set; }
+
+        [Display(Name = "Achternaam")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Achternaam is verplicht")]
+        [DataType(DataType.Text)]
+        public string Achternaam { get; set; }
+    }
+}
