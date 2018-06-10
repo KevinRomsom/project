5aa73aedb80025eaa9a7fcf95942aae594b3685a
kromsom <kevin.romsom@gmail.com>
2018-06-10 21:14:16 +0200

    ada

---
+++ Killerapp/Logic/DrinksLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Models;
+
+namespace Logic
+{
+    public class DrinksLogic
+    {
+        DBDrinksAdmin daldrinks;
+
+        public void insertDrinks(Drinks d)
+        {
+            daldrinks = new DBDrinksAdmin();
+
+            string naam = d.Naam;
+            string categorie = d.Categorie;
+            string soort = d.Soort;
+            string merk = d.Merk;
+            string beschrijving = d.Beschrijving;
+            double prijs = d.Prijs;
+            string foto = d.ImageLink;
+
+            daldrinks.InsertDrinks(naam, merk, soort, categorie, beschrijving, prijs, foto);
+        }
+    }
+}
---
+++ Killerapp/Models/IndexModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Index
+    {
+        public string Naam { get; set; }
+        public string Merk { get; set; }
+        public string Soort { get; set; }
+        public string Categorie { get; set; }
+        public string Beschrijving { get; set; }
+        public double Prijs { get; set; }
+        public string ImageLink { get; set; }
+        public double Korting { get; set; }
+    }
+}
